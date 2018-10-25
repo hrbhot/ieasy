6320b5af4a24970bee55efdbca3890a48acd2844
hrbhot <hrbhot@gmail.com>
2018-10-25 17:14:13 +0200

    修改路径

--- setup_fdfs_server.sh
+++ setup_fdfs_server.sh
@@ -1,4 +1,6 @@
 #启动本地tracker
-fdfs_trackerd ./conf/tracker.conf restart
+#fdfs_trackerd ./conf/tracker.conf restart
+fdfs_trackerd $GOPATH/src/iHome/conf/tracker.conf restart
 #启动本地storage
-fdfs_storaged ./conf/storage.conf restart
+
+fdfs_storaged $GOPATH/src/iHome/conf/storage.conf restart
